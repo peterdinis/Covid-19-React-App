d5f2b26444e5e8397326029b82ad45a1ea0d4654
Peterdinis <peterdinis0@pobox.sk>
2023-01-25 21:17:25 +0100

    feat: Update wrapper component

--- src/components/hero/Wrapper.tsx
+++ src/components/hero/Wrapper.tsx
@@ -1,16 +1,16 @@
 const Wrapper: React.FC = () => {
   return (
-    <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-6 gap-8">
-      <div className="rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
-        <h4>All Stats</h4>
+    <div className="mt-12 container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-6 gap-8">
+      <div className="bg-slate-50 rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
+        <h4 className="text-center mt-8 text-2xl font-bold">All Stats</h4>
       </div>
 
-      <div className="rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
-        <h4>All Deaths</h4>
+      <div className="bg-slate-50 rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
+        <h4 className="text-center mt-8 text-2xl font-bold">All Deaths</h4>
       </div>
 
-      <div className="rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
-        <h4>Specific country info</h4>
+      <div className="bg-slate-50 rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
+        <h4 className="text-center mt-8 text-2xl font-bold">Specific country info</h4>
       </div>
     </div>
   );
