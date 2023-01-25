2446e79178bb041451b135123351ce87585500ea
Peterdinis <peterdinis0@pobox.sk>
2023-01-25 21:25:36 +0100

    feat: Start working on pages

--- src/components/hero/Wrapper.tsx
+++ src/components/hero/Wrapper.tsx
@@ -2,7 +2,9 @@ const Wrapper: React.FC = () => {
   return (
     <div className="mt-12 container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-6 gap-8">
       <div className="bg-slate-50 rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
-        <h4 className="text-center mt-8 text-2xl font-bold">All Stats</h4>
+        <h4 className="text-center mt-8 text-2xl font-bold">
+            <a href="/stats">All Stats</a>
+        </h4>
       </div>
 
       <div className="bg-slate-50 rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
