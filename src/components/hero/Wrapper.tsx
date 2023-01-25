538b25b794299c0d5fe82a3d2c27cc0695002eb9
Peterdinis <peterdinis0@pobox.sk>
2023-01-25 21:44:23 +0100

    feat: UPdating wrapper and create first query

--- src/components/hero/Wrapper.tsx
+++ src/components/hero/Wrapper.tsx
@@ -3,7 +3,7 @@ const Wrapper: React.FC = () => {
     <div className="mt-12 container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pt-6 gap-8">
       <div className="bg-slate-50 rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
         <h4 className="text-center mt-8 text-2xl font-bold">
-            <a href="/stats">All Stats</a>
+            <a href="/stats">All Countries Stats</a>
         </h4>
       </div>
 
