e9c7d5b482ab1f54fca3856dcad5528f71803b95
Peterdinis <peterdinis0@pobox.sk>
2023-01-26 22:37:29 +0100

    foo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { AllStatsPage, LandingPage } from "./pages";
+import { AllStatsPage, LandingPage, VaccinatePage } from "./pages";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/vaccinate" element={<VaccinatePage />} />
       <Route path="/stats" element={<AllStatsPage />} />
     </Routes>
   )
