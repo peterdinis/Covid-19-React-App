a3b5283542c4214473d03d2b96fbaae1474b0b5f
Peterdinis <peterdinis0@pobox.sk>
2023-01-27 15:09:18 +0100

    feat: Update api folder

--- src/api/queries/vaccinateQueries.ts
+++ src/api/queries/vaccinateQueries.ts
@@ -3,3 +3,5 @@ import axios from "axios";
 const api = axios.create({
     baseURL: import.meta.env.VITE_COVID_URL as string
 })
+
+export const getAllVaccines = () => api.get("vaccine").then((res) => res.data);
--- src/api/queries/variantQueries.ts
+++ src/api/queries/variantQueries.ts
@@ -3,3 +3,5 @@ import axios from "axios";
 const api = axios.create({
     baseURL: import.meta.env.VITE_COVID_URL as string
 })
+
+export const getAllVariants = () => api.get("variants").then((res) => res.data);
