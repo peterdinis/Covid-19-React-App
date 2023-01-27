89449e849aed48092ac4e5ddf75577ee204052b5
Peterdinis <peterdinis0@pobox.sk>
2023-01-27 14:33:52 +0100

    FOO

--- src/components/hero/Wrapper.tsx
+++ src/components/hero/Wrapper.tsx
@@ -9,7 +9,7 @@ const Wrapper: React.FC = () => {
 
       <div className="bg-slate-50 rounded border-gray-300  dark:border-gray-700 border-dashed border-2 h-24">
         <h4 className="text-center mt-8 text-2xl font-bold">
-            <a href="/deaths">All Deaths</a>
+            <a href="/variatns">All Variants</a>
         </h4>
       </div>
 
